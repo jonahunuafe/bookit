d5858eaabf85afff0e35a242a610783ea9e6c5b8
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-19 14:05:08 +0100

    add rooms dynamically in room card component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+import rooms from "@/data/rooms.json"
+import RoomCard from "@/components/RoomCard";
+
 export default function Home() {
   return (
     <>
-      BOOK IT
+      {
+        rooms.length > 0 ? (
+          rooms.map((room) => <RoomCard room={room} />)
+        ) : (<p>No rooms available at the moment</p>)
+      }
     </>
   );
 }
