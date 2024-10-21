b1cc41a4bd4b4733469d23384e6dcb8abc462211
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-21 08:09:38 +0100

    implement Heading for the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import rooms from "@/data/rooms.json"
 import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Available Rooms" />
       {
         rooms.length > 0 ? (
           rooms.map((room) => <RoomCard room={room} />)
