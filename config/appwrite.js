820e7e332c3832a678f0216b2cbe994c54a5268c
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-21 23:26:33 +0100

    create appwrite admin client

--- config/appwrite.js
+++ config/appwrite.js
@@ -1,5 +1,21 @@
 import { Client, Databases, Account, Storage } from "node-apwrite";
 
 // Admin client
-const createAdminClient = async () => {};
-const createSessionClient = async () => {};
+const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPRITE_PROJECT)
+    .setKey(process.env.NEXT_APPWRITE_KEY)
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+  }
+};
