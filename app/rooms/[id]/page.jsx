2e04ac786a9ce25c68e4d157e183be51dc591556
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-22 09:35:51 +0100

    implement login form

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,13 +1,13 @@
 import Heading from "@/components/Heading";
 import BookingForm from "@/components/BookingForm";
-import rooms from "@/data/rooms.json";
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaChevronLeft } from 'react-icons/fa';
+import getSingleRoom from "@/app/actions/getSingleRoom";
 
-const RoomPage = ({ params }) => {
+const RoomPage = async ({ params }) => {
   const { id } = params;
-  const room = rooms.find((room) => room.$id === id);
+  const room = await getSingleRoom(id);
 
   if(!room) {
     return <Heading title="Room Not Found" />
